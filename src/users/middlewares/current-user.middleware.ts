590b02156b08ee85c77b6147d273d517b515376a
Fayez Ghazzawi <fayez.ghazzawi@outlook.com>
2023-11-06 11:45:36 +0400

    Fixing a Type Definition Error

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -14,10 +23,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { userId } = req.session || {};
 
     if (userId) {
-      const user = await this.usersService.findOne(userId);
-
-      // @ts-ignore
-      req.currentUser = user;
+      req.currentUser = await this.usersService.findOne(userId);
     }
 
     next();
