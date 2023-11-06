350b41203625ae3bacf64f87af3c92a83a79b9e5
Fayez Ghazzawi <fayez.ghazzawi@outlook.com>
2023-11-06 10:54:55 +0400

    Adding in Report Approval

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { CreateReportDto } from './dtos/create-report.dto';
-import { ReportsService } from './reports.service';
-import { AuthGuard } from '../guards/auth.guard';
-import { CurrentUser } from '../users/decorators/current-user.decorators';
-import { User } from '../users/user.entity';
-import { ReportDto } from './dtos/report.dto';
-import { Serialize } from '../interceptors/serialize.interceptor';
+import { Body, Controller, Param, Patch, Post, UseGuards } from "@nestjs/common";
+import { CreateReportDto } from "./dtos/create-report.dto";
+import { ReportsService } from "./reports.service";
+import { AuthGuard } from "../guards/auth.guard";
+import { CurrentUser } from "../users/decorators/current-user.decorators";
+import { User } from "../users/user.entity";
+import { ReportDto } from "./dtos/report.dto";
+import { Serialize } from "../interceptors/serialize.interceptor";
+import { ApproveReportDto } from"./dtos/approve-report.dto"';
 
 @Controller('reports')
 export class ReportsController {
@@ -16,5 +17,9 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
+  }
+
+  @Patch("/:id")
+  approveReport(@Param("id") id: string, @Body() body: ApproveReportDto) {
   }
 }
