6b98532c6b0ff1bf7c2bccb8cd529e27ede5638f
Fayez Ghazzawi <fayez.ghazzawi@outlook.com>
2023-11-06 11:41:32 +0400

    Use Globally Identified Middlewares

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,12 +1,19 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from "@nestjs/common";
-import { CreateReportDto } from "./dtos/create-report.dto";
-import { ReportsService } from "./reports.service";
-import { AuthGuard } from "../guards/auth.guard";
-import { CurrentUser } from "../users/decorators/current-user.decorators";
-import { User } from "../users/user.entity";
-import { ReportDto } from "./dtos/report.dto";
-import { Serialize } from "../interceptors/serialize.interceptor";
-import { AdminGuard } from "../guards/admin.guard";
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorators';
+import { User } from '../users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -19,11 +26,8 @@ export class ReportsController {
     return this.reportsService.create(body, user);
   }
 
-  @Patch"/:id"')
-  "id"') id: string, @Body() body: ApproveReportDto) {return
-    this reportsService.approved.
-@UseGuards(AdminGuard)
-  approveReport(@Param.
-changeApproval(id, body);
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
   }
 }
